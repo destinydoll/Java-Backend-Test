83a5905571b8c92c70c3045be41051191b26a2bf
destinydoll <sriyaphai.tospon@gmail.com>
2020-12-18 12:58:12 +0700

    test2

--- src/main/java/com/example/test/TestApplication.java
+++ src/main/java/com/example/test/TestApplication.java
@@ -4,13 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EntityScan(basePackages = { "com.example.test.DTO" })
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-public class TestApplication {
+public class TestApplication extends SpringBootServletInitializer{
 
 	@Bean
 	public PasswordEncoder encoder() {
